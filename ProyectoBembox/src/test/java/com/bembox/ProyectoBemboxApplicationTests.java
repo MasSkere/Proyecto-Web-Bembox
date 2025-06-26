97e385ca16ebf3f4d336e3f6f4d70a86943bc618
MasSkere <ronadlmartinez996@gmail.com>
2025-06-26 06:04:58 -0500

    Primer Commit Proyecto Web Ronal

---
+++ ProyectoBembox/src/test/java/com/bembox/ProyectoBemboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bembox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoBemboxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
